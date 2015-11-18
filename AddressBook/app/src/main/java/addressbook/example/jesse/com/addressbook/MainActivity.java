0e67898cf17e5227fe9e09eece85239418a41024
jesse watson <jesse watson>
2015-11-18 16:41:31 -0500

    "Initial commit of the MainActivity"

---
+++ AddressBook/app/src/main/java/addressbook/example/jesse/com/addressbook/MainActivity.java
@@ -0,0 +1,13 @@
+package addressbook.example.jesse.com.addressbook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
