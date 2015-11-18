60a8e56abdecc1bb9d0b4af4f7562b73c6ca70f9
Jesse Watson <jessewatson63@yahoo.com>
2015-11-18 18:00:58 -0500

    added icon and button to switch to view_contacts page.

--- AddressBook/app/src/main/java/addressbook/example/jesse/com/addressbook/MainActivity.java
+++ AddressBook/app/src/main/java/addressbook/example/jesse/com/addressbook/MainActivity.java
@@ -1,13 +1,31 @@
 package addressbook.example.jesse.com.addressbook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button viewContactsButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewContactsButton = (Button) findViewById(R.id.viewContactsButton);
+
+        viewContactsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent getNameScreenIntent = new Intent(getBaseContext(), SecondScreen.class);
+
+                final int result = 1;
+
+                getNameScreenIntent.putExtra("callingActivity", "MainActivity");
+
+                startActivity(getNameScreenIntent);
+            }
+        });
     }
 }
---
+++ AddressBook/app/src/main/java/addressbook/example/jesse/com/addressbook/SecondScreen.java
@@ -0,0 +1,20 @@
+package addressbook.example.jesse.com.addressbook;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by jesse on 11/18/2015.
+ */
+public class SecondScreen extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_contacts);
+
+
+
+    }
+}
